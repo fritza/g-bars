bb7bcf987fd60bb6a5f9a31b37be610ef65eb0c4
Fritz Anderson <fritza@uchicago.edu>
2022-06-30 10:36:13 -0500

    Wrap the root view in a TabView.

--- G Bars/G_BarsApp.swift
+++ G Bars/G_BarsApp.swift
@@ -7,11 +7,24 @@
 
 import SwiftUI
 
+
+
 @main
 struct G_BarsApp: App {
+    @State var selectedTab: Int = 0
     var body: some Scene {
+        // Create a WindowGroup depicting the single view
         WindowGroup {
-            ContentView()
+            TabView(selection: $selectedTab) {
+                ContentView()
+                    .tabItem {
+                        Label("Acceleration", systemImage: "arrow.up.arrow.down")
+                    }
+                Text("For rent")
+                    .tabItem {
+                        Label("Empty", systemImage: "square")
+                    }
+            }
         }
     }
 }
