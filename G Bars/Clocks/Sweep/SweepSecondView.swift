2faf51730b7629c497db64166bfef2fddd663ead
Fritz Anderson <fritza@uchicago.edu>
2022-07-20 17:10:56 -0500

    Have SweepSecondView pull its former hasCompleted in favor of .isRunning on the controller.

--- G Bars/Clocks/Sweep/SweepSecondView.swift
+++ G Bars/Clocks/Sweep/SweepSecondView.swift
@@ -10,55 +10,59 @@ import Combine
 
 /// A `View` that displays a circle containing a sweep-second hand and a digit, representing a countdown in seconds.
 struct SweepSecondView: View {
+    @Environment(\.colorScheme) private static var colorScheme: ColorScheme
     @EnvironmentObject /*private*/ var controller: CountdownController
 
-    @State private var hasCompleted: Bool = false
     @State private var seconds: Int
 
     private var cancellables: Set<AnyCancellable> = []
 
     init() {
-        hasCompleted = false
+//        hasCompleted = false
         seconds = 0
-        controller.$seconds.assign(to: \.seconds, on: self).store(in: &cancellables)
+//        controller.$seconds.assign(to: \.seconds, on: self).store(in: &cancellables)
     }
 
     func numericOverlay(representing: Int, edge: CGFloat) -> some View {
         Text("\(representing)")
-            .font(.system(size: edge, weight: .semibold, design: .default))
+            .font(.system(size: edge, weight: .semibold))
+            .monospacedDigit()
     }
 
     var body: some View {
         GeometryReader { proxy in
             VStack {
+                // A circle/bezel, under a sweep hand, under numeral seconds.
                 ZStack(alignment: .center) {
+                    // TODO: Should clock face → its own view?
                     Circle()
                         .stroke(lineWidth: 1.0)
                         .foregroundColor(.gray)
-                    SubsecondHandView()
 
+                    SubsecondHandView()
+                        .foregroundColor((Self.colorScheme == .light) ? .black : .gray)
+                    
                     numericOverlay(
-                        representing: hasCompleted ?
-                        0 :
-                            controller.seconds+1,
-                        edge: proxy.size.short * 0.6)
+                        representing: controller.isRunning ?
+                        controller.seconds+1 :
+                            0,
+                        edge: proxy.size.short * 0.6
+                    )
                 }
                 .navigationTitle("Seconds")
-
-                // TODO: Track a published Bool for completion.
-                .onReceive(controller.$isRunning) { stillRunning in
-                    hasCompleted = true
-                }
-                .frame(width: proxy.size.short * 0.95,
+                .frame(width:  proxy.size.short * 0.95,
                        height: proxy.size.short * 0.95,
                    alignment: .center)
                 Spacer()
-                Button("Cancel") {
-                    controller.stopCounting(timeRanOut: false)
+                Button(controller.isRunning ? "Cancel" : "Start") {
+                    if controller.isRunning {
+                        controller.stopCounting(timeRanOut: false)
+                    }
+                    else {
+                        controller.reassemble(newDuration: 5.0)
+                        controller.startCounting()
+                    }
                 }
-            }
-            .onAppear {
-                controller.startCounting()
             }
         }
     }
@@ -71,7 +75,7 @@ struct SweepSecondView_Previews: PreviewProvider {
                 .frame(width: 300)
         }
         .environmentObject(
-            CountdownController(forCountdown: true)
+            CountdownController(duration: 10)
         )
     }
 }
