84626badb4ed3bf82967f1d1a2903d82533f9a54
Fritz Anderson <fritza@uchicago.edu>
2022-07-29 15:34:27 -0500

    AtEnvironment.swift defines some @Environment properties. No code as yet.
    Move SimpleBarView and HorizontalBar into the Accelerometry group

---
+++ G Bars/AtEnvironment.swift
@@ -0,0 +1,20 @@
+//
+//  AtEnvironment.swift
+//  G Bars
+//
+//  Created by Fritz Anderson on 7/29/22.
+//
+
+import Foundation
+
+/**
+ @Environment values for what otherwise would be observable globals.
+
+ ## Topics
+
+ ### Speech
+ - `speechActive`
+ */
+
+
+
