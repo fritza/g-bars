0bef582f4cadd9f230bc28821bcf81d31b49ea91
Fritz Anderson <fritza@uchicago.edu>
2022-08-07 21:35:17 -0500

    Add MinSecAndFraction and TimeReader as a cleaner Combine stack without a "countdown controller."
    MinSecAndFraction -> struct containing the components of the time remaining in the countdown.
    TimeReader -> operates a Combine stack for turning Timer publication into MinSecAndFraction.

---
+++ G Bars/Clocks/Support/MinSecAndFraction.swift
@@ -0,0 +1,80 @@
+//
+//  MinSecAndFraction.swift
+//  G Bars
+//
+//  Created by Fritz Anderson on 8/5/22.
+//
+
+import Foundation
+
+
+/// A single struct embodying a minute, second, and fraction for some interval.
+public struct MinSecAndFraction: Hashable {
+    public let minute  : Int
+    public let second  : Int
+    public let fraction: TimeInterval
+
+    /// Create a `MinSecAndFraction` from its components.
+    /// - Parameters:
+    ///   - minute: The truncated number of minutes in the interval
+    ///   - second: The truncated number of seconds within the minute
+    ///   - fraction: The fraction of the current second, pinned to `(0..<1)`
+    /// - note: No attempt is made to validate or reconcile the parameters.
+    public init(minute: Int, second: Int, fraction: TimeInterval = 0.0) {
+        (self.minute, self.second, self.fraction) =
+        (minute, second, fraction)
+    }
+
+    public init(interval: TimeInterval) {
+        let intInterval = Int(round(interval))
+        self.init(minute: intInterval / 60,
+                  second: intInterval % 60,
+                  fraction: interval - trunc(interval))
+    }
+
+    /// Whether all components are zero. Prefer this to comparison to `.zero`.
+    public var isZero: Bool {
+        minute == 0 && second == 0 && fraction == 0.0
+    }
+
+    /// A copy of this struct with the `fraction` component set to a new value
+    /// - Parameter fraction: The `fraction` component for the copy
+    /// - Returns: A `MinSecAndFraction` with `self`'s `minute` and `second`, but `fraction` taken from the parameter.
+    public func with(fraction: Double) -> MinSecAndFraction {
+        return MinSecAndFraction(minute: minute, second: second, fraction: fraction)
+    }
+
+    /// The `MinSecAndFraction` that has all zeros for its components.
+    public static let zero = MinSecAndFraction(minute: 0, second: 0, fraction: 0.0)
+}
+
+
+extension MinSecAndFraction: CustomStringConvertible {
+    /// `CustomStringConvertible` adoption. Displays all three components as in `01:15 + 0.685`.
+    public var description: String {
+        self.clocked + " + \(self.fraction.pointThree)"
+    }
+
+    /// Displays the minute and second  as in `01:15`, no fraction.
+    public var clocked: String {
+        "\(self.minute.twoZeros):\(self.second.twoZeros)"
+    }
+
+    /// The interval written out as it would be spoken. "Minutes" and "seconds" are pluralized as needed; omitted if zero; the return value is `zero` if all components are zero.
+    public var spoken: String {
+        if self.isZero { return "zero" }
+        var partial = ""
+        if self.minute > 0 {
+            let unitM = (self.minute > 1) ? "minutes" : "minute"
+            partial = "\(self.minute) \(unitM)"
+        }
+
+        guard self.second != 0 else { return partial }
+
+        if self.second > 0 {
+            let unitS = (self.second > 1) ? "seconds" : "second"
+            partial += " \(self.second) \(unitS)"
+        }
+        return partial
+    }
+}
---
+++ G Bars/Clocks/Support/TimeReader.swift
@@ -0,0 +1,138 @@
+//
+//  TimeReader.swift
+//  G Bars
+//
+//  Created by Fritz Anderson on 8/5/22.
+//
+
+import Foundation
+import Combine
+
+
+
+final class TimeReader: ObservableObject {
+    enum TerminationErrors: Error, CustomStringConvertible {
+        case cancelled
+        case expired
+
+        var description: String {
+            switch self {
+            case .expired  : return "the timer expired"
+            case .cancelled: return "the count was cancelled"
+            }
+        }
+    }
+
+    enum TimerStatus: String {
+        case ready, running, cancelled, expired
+    }
+
+
+    @Published var currentTime: MinSecAndFraction = .zero
+    @Published var status: TimerStatus = .ready
+
+    var startingDate, endingDate: Date
+    let totalInterval: TimeInterval
+    let tickInterval: TimeInterval
+    let tickTolerance: TimeInterval
+
+    var timeSubject = PassthroughSubject<MinSecAndFraction, Never>()
+    var secondsSubject = PassthroughSubject<Int, Never>()
+
+    init(interval: TimeInterval, by tickSize: TimeInterval = 0.01) {
+        tickInterval = tickSize
+        tickTolerance = tickSize / 20.0
+
+        let currentDate = Date()
+        totalInterval = interval
+        startingDate = currentDate
+        endingDate = Date().addingTimeInterval(interval)
+    }
+
+
+    var sharedTimer: AnyPublisher<MinSecAndFraction, Error>!
+    var timeCancellable: AnyCancellable!
+    var secondsCancellable: AnyCancellable!
+    func cancel() {
+        assert(status == .running)
+        status = .cancelled
+
+        timeCancellable = nil
+        secondsCancellable = nil
+        sharedTimer = nil
+    }
+
+    func start() {
+        assert(status == .ready,
+        "attempt to restart a timer")
+
+        startingDate = Date()
+        endingDate = Date().addingTimeInterval(totalInterval)
+        status = .running
+        sharedTimer = setUpCombine().share().eraseToAnyPublisher()
+
+        timeCancellable = sharedTimer
+            .sink { completion in
+                switch completion {
+                case .finished: print("Tell the world it worked.")
+                    self.status = .expired
+                case .failure(let error):
+                    guard let err = error as? TerminationErrors else {
+                        print("other error: \(error).")
+                        return
+                    }
+                    switch err {
+                    case .expired: print("Clock ran out")
+                    case .cancelled: print("was cancelled")
+                        self.status = .cancelled
+                    }
+                }
+            } receiveValue: { msf in
+                self.timeSubject.send(msf)
+            }
+
+        secondsCancellable = sharedTimer
+            .map { $0.second }
+            .removeDuplicates()
+            .assertNoFailure()
+            .sink { secInteger in
+                self.secondsSubject.send(secInteger)
+            }
+    }
+
+    static let roundingScale = 100.0
+
+    private func setUpCombine() -> AnyPublisher<MinSecAndFraction, Error>
+    {
+        let retval = Timer.publish(every: tickInterval,
+                                   tolerance: tickTolerance,
+                                   on: .main, in: .common)
+            .autoconnect()
+            .tryMap {
+                (date: Date) -> TimeInterval in
+                guard date <= self.endingDate else {
+                    self.status = .expired
+                    throw TerminationErrors.expired
+                }
+                return self.endingDate.timeIntervalSince(date)
+            }
+            .map { rawInterval in
+                let scaled = Self.roundingScale * rawInterval
+                let trimmed = round(scaled)
+                let rescaled = trimmed / Self.roundingScale
+                return rescaled
+            }
+            .map {
+                (tInterval: TimeInterval) -> MinSecAndFraction in
+                let intInterval = Int(round(tInterval))
+                return MinSecAndFraction(
+                    minute: intInterval / 60,
+                    second: intInterval % 60,
+                    fraction: tInterval - trunc(tInterval)
+                )
+            }
+            .eraseToAnyPublisher()
+        return retval
+    }
+}
+
