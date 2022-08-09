43928ddcce1c0a1a99b85c11662cb3859f83cf78
Fritz Anderson <fritza@uchicago.edu>
2022-08-09 14:28:23 -0500

    Rename TaskInterstitialText to InterstitialInfo, TaskInterstitialList to InterstitialList. Add a Resources/ group and JSON for interstitials.

---
+++ G Bars/InterstitialInfo.swift
@@ -0,0 +1,130 @@
+//
+//  InterstitialInfo.swift
+//  G Bars
+//
+//  Created by Fritz Anderson on 8/9/22.
+//
+
+import Foundation
+
+/// An element of ``InterstitialList``, providing common types of content for an interstitial view.
+///
+/// The expected use decodes `InterstitialInfo` from a JSON file. It is _not_ possible to initialize one directly.
+struct InterstitialInfo: Codable, Hashable, Identifiable {
+    /// Ths ID for this page, automatically assigned, and **one-based**.
+    public let id: Int
+    /// The introductory text for the page
+    public let intro: String
+    // FIXME: Shouldn't have just one text area.
+    /// The label on the regular "proceed" `Button` at bottom.
+    public let proceedTitle: String
+    public let pageTitle: String
+    /// The SF Symbols name for the image to display in the middle of the page.
+    public let systemImage: String?
+
+    /// Element-wise initialization.
+    ///
+    /// `InterstitialInfo` has no public initializers.
+    private init(id: Int, intro: String, proceedTitle: String, pageTitle: String, systemImage: String?) {
+        self.id = id
+        self.intro = intro
+        self.proceedTitle = proceedTitle
+        self.pageTitle = pageTitle
+        self.systemImage = systemImage
+    }
+
+    /// Initialize an `InterstitialInfo` from its `Decodable` content, plus an `Int` ID supplied by ``InterstitialList``.
+    ///
+    /// `InterstitialInfo` has no public initializers.
+    /// - Parameters:
+    ///   - stub: The `Decodable` (``TaskInterstitialDecodable``) content for the interstitial page.
+    ///   - id: The ID assigned from an `InterstitialList`
+    fileprivate init(_ stub: TaskInterstitialDecodable,
+                     id: Int) {
+        self.init(id: id,
+                  intro: stub.intro.addControlCharacters,
+                  proceedTitle: stub.proceedTitle,
+                  pageTitle: stub.pageTitle,
+                  systemImage: stub.systemImage)
+    }
+}
+
+/// Content for the page _except_ for the ID, which is assigned at decoding time as JSON array order **plus one**.
+///
+/// See ``InterstitialInfo`` for details on the properties.
+fileprivate struct TaskInterstitialDecodable: Codable {
+    let intro: String
+    // TODO: Should proceedTitle ever be nil?
+    let proceedTitle: String
+    let pageTitle: String
+    let systemImage: String?
+
+    // TODO: See if this is ever needed.
+    var unescaped: TaskInterstitialDecodable {
+        return TaskInterstitialDecodable(intro: self.intro.addControlCharacters,
+                                         proceedTitle: proceedTitle,
+                                         pageTitle: pageTitle,
+                                         systemImage: systemImage)
+    }
+}
+
+/// A collection of ``InterstitialInfo``,  as read from a JSON file.
+///
+/// This is expected to be the content of all interstitials within a task. Clients are responsible for matching indices to the needs of a particular interstitial.
+///
+/// `InterstitialList` is initialized from a JSON file, given the file's basename. The JSON must not attempt to specify IDs; this will be done at init time.
+/// - note: All methods expect a 1-based index.
+struct InterstitialList: Codable {
+    typealias Element = InterstitialInfo
+    typealias Index   = Int
+
+    // Coding
+    enum CodingKeys: String, CodingKey {
+        case baseName, interstitials
+    }
+    static let decoder = JSONDecoder()
+
+
+    let baseName: String
+    let interstitials: [InterstitialInfo]
+
+    /// The `InterstitialInfo`, if any, having the supplied ID.
+    /// - note: This is an _ID,_ not the collection index used for subscripting.
+    /// - parameter target: The ID to search for.
+    /// - returns: `InterstitialInfo` for the element with that ID, or `nil` if there is none.
+    func item(forID target: Int) -> InterstitialInfo? {
+        let retval = interstitials
+            .first(where: { $0.id == target } )
+        return retval
+    }
+
+    /// Load the list of `InterstitialInfo` from a (base)named `Bundle` file.
+    ///
+    /// Elements in the file will not specify IDs; they identify by their order in the `json` file. This initializer assigns each an `id`  of file order + 1.
+    /// - Parameter baseName: The base name of the file to decode, e.g. `mumble` for `mumble.json`.  The source file must have the `json` extension.
+    init(baseName: String) {
+        self.baseName = baseName
+        // Fill in the interstital list, if any
+        if let url = Bundle.main.url(forResource: baseName, withExtension: "json"),
+           let jsonData = try? Data(contentsOf: url),
+           let rawList = try? Self.decoder
+            .decode([TaskInterstitialDecodable].self,
+                    from: jsonData) {
+            let idedList = rawList.enumerated()
+                .map { (idNum, content) in
+                    return InterstitialInfo(content, id: idNum+1)
+                }
+            interstitials = idedList
+        }
+        else {
+            interstitials = []
+        }
+    }
+}
+
+// - MARK: RandomAccessCollection adoption
+extension InterstitialList: RandomAccessCollection {
+    var startIndex: Int { 1 }
+    var endIndex: Int { interstitials.count-1 }
+    subscript(index: Int) -> Element { interstitials[index-1] }
+}
