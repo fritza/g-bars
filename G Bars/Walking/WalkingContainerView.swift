e84c43f963fe371c60016847ef764a06003fbee8
Fritz Anderson <fritza@uchicago.edu>
2022-08-18 15:42:21 -0500

    WalkingContainerView: refactor the huge elements of the VStack into funcs -> some View.

--- G Bars/Walking/WalkingContainerView.swift
+++ G Bars/Walking/WalkingContainerView.swift
@@ -12,145 +12,168 @@ private let instructionList     = InterstitialList(baseName: "walk-intro"       
 private let mid_instructionList = InterstitialList(baseName: "second-walk-intro")
 private let end_walkingList     = InterstitialList(baseName: "usability-intro"  )
 
-
-private struct ContainedView: View {
-    let systemName: String
-    let text: String
-    let closeout: () -> Void
-
-    internal init(imageName: String, text: String, closeout: @escaping () -> Void) {
-        self.systemName = imageName
-        self.text = text
-        self.closeout = closeout
-    }
-
-    var body: some View {
-        VStack(alignment: .center) {
-            Image(systemName: self.systemName)
-                .resizable()
-                .scaledToFit()
-                .frame(height: 100)
-                .foregroundColor(.accentColor)
-                .symbolRenderingMode(.hierarchical)
-
-            Text(self.text)
-            Button("Continue", action: closeout)
-        }
-    }
-}
-
-enum WalkingState: String, Hashable, CaseIterable {
-    case interstitial_1, countdown_1, walk_1
-    case interstitial_2, countdown_2, walk_2
-    case end_interstitial
-}
-
 struct WalkingContainerView: View {
     @State var state: WalkingState? = .interstitial_1
 
+    @State private var shouldShowActivity = false
+    @State private var walkingData = Data()
+
     var body: some View {
         NavigationView {
-            // LazyVStack did not prevent double-initialization of the target views (and thereby creation of multiple timers, some of which expire (second walk) while the first walk is run.
-            // Removing VStack didn't display countdown_1 when the intro was completed: nav title appeared, content did not.
-            // removing .hidden() from interstitial_1 and countdown_1 cases had no effect (maybe fewer initailizations?)
-
-            // Okay: Making sure all of these wait at least until onAppear before
-            // they start counting fixed the multiple concurrent timers.
-            // TODO: Prevent creation of redundant timers.
-
             VStack {
-                // MARK: Intro
-                NavigationLink(
-                    "SHOULDN'T SEE (interstitial_1)",
-                    tag: WalkingState.interstitial_1, selection: $state) {
-                    InterstitalPageTabView(listing: instructionList, selection: 1) {
-                        self.state = .countdown_1
-                    }.padding()
+                interstitial_1View()
+                countdown_1View()
+                walk_1View()
+                interstitial_2View()
+                countdown_2View()
+                walk_2View()
+                end_interstitialView()
+            }   // VStack
+        }       // NavigationView
+
+        // MARK: Usability
+        .navigationTitle("Walking (beta)")
+    } // body
+}
+
+// MARK: - Walking stages
+extension WalkingContainerView {
+    @ViewBuilder
+    func interstitial_1View() -> some View {
+        NavigationLink(
+            "SHOULDN'T SEE (interstitial_1)",
+            tag: WalkingState.interstitial_1, selection: $state) {
+                InterstitalPageTabView(listing: instructionList, selection: 1) {
+                    self.state = .countdown_1
+                }.padding()
                     .navigationBarBackButtonHidden(true)
-                }
-
-                // MARK: countdown (1)
-                NavigationLink(
-                    "SHOULDN'T SEE (countdown_1)",
-                    tag: WalkingState.countdown_1, selection: $state) {
-                    SweepSecondView(duration: sweep_TMP_Duration) {
-                        state = .walk_1
-                    }.padding()
-                    .navigationBarBackButtonHidden(true)
-                }
-//                .hidden()
-
-                // MARK: Walk (1)
-                NavigationLink(
-                    "SHOULDN'T SEE (walk_1)",
-                    tag: WalkingState.walk_1, selection: $state) {
-                    DigitalTimerView(duration: countdown_TMP_Duration) {
-                        state = .interstitial_2
-                    }.padding()
-                    .navigationBarBackButtonHidden(true)
-                }
-//                .hidden()
-
-
-                // MARK: Mid
-                NavigationLink(
-                    "SHOULDN'T SEE (interstitial_2)",
-                    tag: WalkingState.interstitial_2, selection: $state) {
-                        InterstitalPageTabView(listing: mid_instructionList, selection: 1) {
-                            self.state = .countdown_2
-                        }.padding()
-                    .navigationBarBackButtonHidden(true)
-                }
-                //                .hidden()
-
-                // MARK: Countdown (2)
-                NavigationLink(
-                    "SHOULDN'T SEE (countdown_2)",
-                    tag: WalkingState.countdown_2, selection: $state) {
-                        SweepSecondView(duration: sweep_TMP_Duration) {
-                            state = .walk_2
-                        }.padding()
-                        .navigationBarBackButtonHidden(true)
-                    }
-                //                .hidden()
-
-
-                // MARK: Walking (2)
-                NavigationLink(
-                    "SHOULDN'T SEE (walk_2)",
-                    tag: WalkingState.walk_2, selection: $state) {
-                        DigitalTimerView(duration: 71.0) {
-                            state = .end_interstitial
-                        }.padding()
-                        .navigationBarBackButtonHidden(true)
-                    }
-                //                .hidden()
-
-
-                // MARK: End
-                NavigationLink(
-                    "SHOULDN'T SEE (end_interstitial)",
-                    tag: WalkingState.end_interstitial, selection: $state) {
-                        InterstitalPageTabView(
-                            listing: end_walkingList,
-                            selection: 1) {
-                                self.state = .interstitial_1
-                            }.padding()
-                            .navigationBarBackButtonHidden(true)
-                    }
-                //                .hidden()
-
-                // MARK: Usability
-                
             }
-            .navigationTitle("Walking (beta)")
+    }
+
+    @ViewBuilder
+    func countdown_1View() -> some View {
+        NavigationLink(
+            "SHOULDN'T SEE (interstitial_1)",
+            tag: WalkingState.interstitial_1, selection: $state) {
+                InterstitalPageTabView(listing: instructionList, selection: 1) {
+                    self.state = .walk_1
+                }.padding()
+                    .navigationBarBackButtonHidden(true)
+            }
+    }
+
+    @ViewBuilder
+    func walk_1View() -> some View {
+        NavigationLink(
+            "SHOULDN'T SEE (walk_1)",
+            tag: WalkingState.walk_1, selection: $state) {
+                DigitalTimerView(duration: countdown_TMP_Duration) {
+                    // → .interstitial_2
+                    state = .interstitial_2
+                }.padding()
+                    .navigationBarBackButtonHidden(true)
+            }
+        //                .hidden()
+    }
+
+    @ViewBuilder
+    func interstitial_2View() -> some View    {             NavigationLink(
+        "SHOULDN'T SEE (interstitial_2)",
+        tag: WalkingState.interstitial_2, selection: $state) {
+            InterstitalPageTabView(listing: mid_instructionList, selection: 1) {
+                // → .countdown_2
+                self.state = .countdown_2
+            }.padding()
+                .navigationBarBackButtonHidden(true)
         }
+        //                .hidden()
+    }
+
+    @ViewBuilder
+    func mid_instructionListView() -> some View {
+        NavigationLink(
+            "SHOULDN'T SEE (interstitial_2)",
+            tag: WalkingState.interstitial_2, selection: $state) {
+                InterstitalPageTabView(listing: mid_instructionList, selection: 1) {
+                    // → .countdown_2
+                    self.state = .countdown_2
+                }.padding()
+                    .navigationBarBackButtonHidden(true)
+            }
+        //                .hidden()
+    }
+
+    @ViewBuilder
+    func countdown_2View() -> some View {
+        NavigationLink(
+            "SHOULDN'T SEE (countdown_2)",
+            tag: WalkingState.countdown_2, selection: $state) {
+                SweepSecondView(duration: sweep_TMP_Duration) {
+                    // → .walk_2
+                    state = .walk_2
+                }.padding()
+                    .navigationBarBackButtonHidden(true)
+            }
+        //                .hidden()
+    }
+
+    func walk_2View() -> some View {
+        NavigationLink(
+            "SHOULDN'T SEE (walk_2)",
+            tag: WalkingState.walk_2, selection: $state) {
+                DigitalTimerView(duration: countdown_TMP_Duration) {
+                    // → .end_interstitial
+                    state = .end_interstitial
+                }.padding()
+                    .navigationBarBackButtonHidden(true)
+            }
+        //                .hidden()
+    }
+
+    @ViewBuilder
+    func end_interstitialView() -> some View {
+        NavigationLink(
+            "SHOULDN'T SEE (end_interstitial)",
+            tag: WalkingState.end_interstitial, selection: $state) {
+                InterstitalPageTabView(
+                    listing: end_walkingList, selection: 1) {
+                        self.state = .interstitial_1
+                    }.padding() // completion closure for end_walkingList
+                    .navigationBarBackButtonHidden(true)
+            }
     }
 }
 
+// MARK: - Preview
 struct WalkingContainerView_Previews: PreviewProvider {
 
     static var previews: some View {
         WalkingContainerView()
     }
 }
+
+/*      SHOW-ACTIVITY button
+ Button {
+ shouldShowActivity = true
+ }
+ label: { Label(
+ "Tap to Export",
+ systemImage: "square.and.arrow.up")
+ }
+ .buttonStyle(.bordered)
+ */
+
+
+/*
+
+ }   // NavigationView
+ .sheet(isPresented: $shouldShowActivity, content: {
+ ActivityUIController(
+ //                    data: walkingData,
+ data: "01234 N/A 56789".data(using: .utf8)!,
+ text: "01234 N/A 56789"
+ //textEquivalent)
+ )
+ }) // .sheet content
+
+ */
